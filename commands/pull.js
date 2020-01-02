c189e65b54e2bfc7dfe6d136706b0819c1f9a765
samurai boye <samuraistacksmmlol@gmail.com>
2020-01-02 19:36:48 -0400

    pull command (should work)

---
+++ commands/pull.js
@@ -0,0 +1,38 @@
+exports.run = (tohru, msg) => {
+	const cp = require("child_process")
+
+	cp.exec("git -C /home/augu/Tohru pull origin master", (err, out, stderr) => {
+		if(err) {
+			msg.channel.createMessage({embed: {
+				color: 0xff0000,
+				description: `Error while trying to pull:\n\`${err}\``
+			}})
+			return;
+		}
+		msg.channel.createMessage({embed: {
+			color: 0xFAB41D,
+			description: "☁️ Here's your output!",
+			fields: [
+				{
+					"name": "Stdout:",
+					"value": `\`\`\`bash\n${out}\`\`\``
+				},
+				{
+					"name": "Stderr:",
+					"value": `\`\`\`bash\n${stderr}\`\`\``
+				}
+			]
+		}})
+	})
+}
+
+exports.config = {
+	aliases: [],
+	ownerOnly: true
+}
+
+exports.help = {
+	name: "pull",
+	desc: "Pulls the latest changes from github.",
+	usage: "pull"
+}
