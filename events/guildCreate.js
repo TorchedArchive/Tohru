797c0fed6dc70df94315d60125864e2bc7c795ed
samurai boye <samuraistacksmmlol@gmail.com>
2020-01-04 13:16:35 -0400

    add guildcreate event

---
+++ events/guildCreate.js
@@ -0,0 +1,11 @@
+module.exports = (tohru, guild) => {
+	tohru.executeWebhook("663063864589680650", require("../config.json").webhook.quetzalcoatl, {
+		content: "Joined a new guild!",
+		embed: [
+			{
+				"color": 0x0,
+				"description": `${guild.name}`
+			}
+		]
+	})
+}
