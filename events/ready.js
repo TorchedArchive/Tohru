56e44644da61e31c0428a3dae2db3662702a121e
samurai boye <samuraistacksmmlol@gmail.com>
2020-01-04 13:54:38 -0400

    make it send a message when online

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,8 @@
 module.exports = (tohru) => {
     tohru.loggr.log(`I am now online, and in ${tohru.guilds.size} servers.`)
-    tohru.editStatus("online", {
+    tohru.editStatus("online", {game: {
     	name: "over you! | @mention me for help!",
     	type: 3
-    })
+    }})
+    tohru.channels.get("663077472186925066").createMessage("I am up now, and ready to be your dragon maid!")
 }
