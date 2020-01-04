a18d3d4937832a06efc0d41fbfc5d2b655eecafb
samurai boye <samuraistacksmmlol@gmail.com>
2020-01-04 13:33:04 -0400

    make it look nicer

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,10 +1,13 @@
-module.exports = (tohru, guild) => {
+module.exports = (tohru, g) => {
+	const gowner tohru.users.get(g.ownerID)
 	tohru.executeWebhook("663063864589680650", require("../config.json").webhook.quetzalcoatl, {
-		content: "Joined a new guild!",
+		content: "📥 Joined a new guild!",
 		embeds: [
 			{
-				"color": 0x0,
-				"description": `${guild.name}`
+				"color": 0x92fc65,
+				"description": `**Name:** \`${g.name}\`
+				**Members:** \`${g.memberCount}\`
+				Owner: ${gowner.username}#${gowner.discriminator}`
 			}
 		]
 	})
