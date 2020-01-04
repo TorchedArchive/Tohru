52af549dada4d4b8f3bb691f78743abe59330db6
samurai boye <samuraistacksmmlol@gmail.com>
2020-01-04 13:19:38 -0400

    tiny typo

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,7 +1,7 @@
 module.exports = (tohru, guild) => {
 	tohru.executeWebhook("663063864589680650", require("../config.json").webhook.quetzalcoatl, {
 		content: "Joined a new guild!",
-		embed: [
+		embeds: [
 			{
 				"color": 0x0,
 				"description": `${guild.name}`
